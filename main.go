0db211e7f10f59b9350e01970ef4e0b1cb51e3ec
Octavio Montt <montt.octavio@gmail.com>
2025-06-17 13:35:14 +0100

    feat: new commands

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			command := scanner.Text()
-			pokemons := cleanInput(command)
-			fmt.Printf("Your command was: %s\n", pokemons[0])
-		}
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,45 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+type config struct {
+	Next    string
+	Preview string
+}
+
+var BaseConfig config
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandWord := words[0]
+		command, exist := getCommands()[commandWord]
+		if exist {
+			err := command.callback(&BaseConfig)
+			if err != nil {
+				fmt.Printf("Error in callback: %v\n", err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
